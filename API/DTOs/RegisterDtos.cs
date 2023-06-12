26677474788590b6ab765dd2cf4da34ecc3636a6
Qabas-mamari <qabasalmamari@gmail.com>
2023-06-12 17:20:04 +0400

    4.4 Adding validation

--- API/DTOs/RegisterDtos.cs
+++ API/DTOs/RegisterDtos.cs
@@ -1,11 +1,15 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDtos
     {
-        public string Username {get; set;} 
+        [Required]
+        public string Username { get; set; }
 
-        public string Password {get; set;}
+        [Required]
+        public string Password { get; set; }
     }
 }
