2e08116408ecee7c318aa5f154787de8c9439463
Qabas-mamari <qabasalmamari@gmail.com>
2023-07-07 17:00:44 +0400

    5.8 Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
@@ -11,24 +10,13 @@ import { User } from './_models/user';
 
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users: any;
 
   // The constructor is used for initializing the object and setting its internal state.
   // The "http" parameter is automatically assigned to the private property.
-  constructor(private http: HttpClient, private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    //1. The subscribe method is called on the Observable to start listening for the response
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response, // when the request is successful, and it receives the response from the server.
-      error: error => console.log(error), //  if an error occurs during the request
-      complete: () => console.log("Request is complete") // when the request is complete, regardless of success or failure.
-    })
   }
 
   /**
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
   RegisterToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    //1. The subscribe method is called on the Observable to start listening for the response
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response, // when the request is successful, and it receives the response from the server.
+      error: error => console.log(error), //  if an error occurs during the request
+      complete: () => console.log("Request is complete") // when the request is complete, regardless of success or failure.
+    })
   }
 
 }
