760b997fd279830369b23cae66285b3d0a5c5626
Qabas-mamari <qabasalmamari@gmail.com>
2023-07-25 11:09:44 +0400

    8.5 Updating users controller & Adding DTO Members

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,26 +9,22 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context){
-            _context = context;
+        
+        private readonly UserRepository _userRepository;
+        public UsersController(UserRepository userRepository){
+            _userRepository = userRepository;
+            
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-            var users = await _context.Users.ToListAsync();
-            return users;
+            return Ok(await _userRepository.GetUsersAsync());
         }
 
 
-        [HttpGet("{id}")]
-        /**
-        The await keyword is used to asynchronously wait for the completion of the FindAsync operation.
-         By marking the method as async and returning a Task, it allows the method to be awaited by the calling code.
-        */
-        public async Task<ActionResult<AppUser>> GetUser(int id){
-            return await _context.Users.FindAsync(id);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUser(string username){
+            return await _userRepository.GetUserByUsernameAsync(username);
         }
     }
 }
