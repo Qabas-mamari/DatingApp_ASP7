846ae481ffcc3640ebd2dae5059b375e9f6062a8
Qabas-mamari <qabasalmamari@gmail.com>
2023-08-13 10:30:16 +0400

    9.1 dding a member service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = "https://localhost:5001/api/";
-
+  baseUrl = environment.apiUrl
   //Property that is initialized with a new instance of the BehaviorSubject class.
   //The BehaviorSubject is a type of observable that stores the most recent value emitted and allows new subscribers to immediately receive that value.
   private currentUserSource = new BehaviorSubject<User | null>(null);
