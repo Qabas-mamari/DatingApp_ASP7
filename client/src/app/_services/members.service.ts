014aa4740517c54069eae5dccfb20254d4206f74
Qabas-mamari <qabasalmamari@gmail.com>
2023-08-16 10:45:33 +0400

    9.4 Using an interceptor to send the token

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,22 +13,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+ "users", this.getHttpOption());
+    return this.http.get<Member[]>(this.baseUrl+ "users");
   }
 
   getMember(username :string){
-    return this.http.get<Member>(this.baseUrl+"users/"+ username, this.getHttpOption())
+    return this.http.get<Member>(this.baseUrl+"users/"+ username)
   }
 
-  getHttpOption(){
-    const userString = localStorage.getItem("user");
-    if(!userString) return;
-
-    const user = JSON.parse(userString);
-    return{
-      headers : new HttpHeaders({
-        Authorization : "Bearer "+ user.token
-      })
-    }
-  }
 }
